8464e84a8b320f760310ad91498c61351c02c0d6
Gremaud Lucien <lucien.gremaud@gmail.com>
2018-10-17 23:39:29 +0200

    added clear button and changing board size

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-function board(size){
+function createBoard(size){
     const board = document.querySelector(".board");
-    board.style.gridAutoColumns = "50px";
-    board.style.gridAutoRows = "50px";
+    board.style.gridAutoColumns = 800/size;
+    board.style.gridAutoRows = 800/size;
 
     for(let x=0; x<size; x++){
         for(let y=0; y<size; y++){
             let item = document.createElement("div");
-
-            item.addEventListener("mouseenter", () => {
-                item.style.background = "black";
-            })
 
             //item.style.border = "1px solid black"
             item.style.gridColumn = x+1;
@@ -20,4 +16,26 @@ function board(size){
     }
 }
 
-board(16);
+function turnBlack(){
+    const items = document.querySelectorAll(".board div");
+
+    items.forEach((item) => {
+        item.addEventListener("mouseenter", () => {
+            item.classList.add("black");
+        })
+    })
+}
+
+function clearBoard(){
+    const board = document.querySelector(".board");
+    while(board.firstChild) {
+        board.removeChild(board.firstChild);
+    }    
+
+    let newSize = prompt("New Board Size");
+    createBoard(newSize);
+    turnBlack();
+}
+
+createBoard(24);
+turnBlack();
